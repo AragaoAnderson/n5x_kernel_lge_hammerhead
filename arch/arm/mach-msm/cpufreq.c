08eafdcc31e576d6d0c7062e684626daf519a7c2
AragaoAnderson <andersonaragao@me.com>
2014-04-12 14:59:21 -0300

    Change default max screen off frequency to 1190400 and set enabled by default

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -67,8 +67,8 @@ static DEFINE_PER_CPU(struct cpufreq_work_struct, cpufreq_work);
 static struct workqueue_struct *msm_cpufreq_wq;
 
 /* maxscroff */
-uint32_t maxscroff_freq = 729600;
-uint32_t maxscroff = 0;
+uint32_t maxscroff_freq = 1190400;
+uint32_t maxscroff = 1;
 
 struct cpufreq_suspend_t {
 	struct mutex suspend_mutex;
